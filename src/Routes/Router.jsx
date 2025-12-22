2c39662f4c5f8f63296d86fdbec02d2d1f6203ea
Aniket Kadam <anikadamg@gmail.com>
2025-12-22 20:19:16 +0530

    Rename for fix

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import Home from "../Components/Main/Home";
 import Services from "../Components/Main/Services";
-import Blog from "../Components/Main/Blog";
+import Affiliates from "../Components/Main/Blog";
 import About from "../Components/Main/About";
 import PrivacyPolicy from "../Components/Foooter/privacyPolicy";
 import RefundPolicy from "../Components/Foooter/refundPolicy";
@@ -53,7 +53,7 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="services" element={<Services />} />
-        <Route path="affiliates" element={<Blog />} />
+        <Route path="affiliates" element={<Affiliates />} />
         <Route path="terms&condition" element={<TermsCondition />} />
         <Route path="refundPolicy" element={<RefundPolicy />} />
         <Route path="privacyPolicy" element={<PrivacyPolicy />} />
